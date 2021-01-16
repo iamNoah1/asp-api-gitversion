f41f1e9731734e2bce269d6d8f4049d7d79ea7ee
Noah Ispas <noi@dataone.de>
2021-01-16 13:03:35 +0100

    initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Api.Template
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("hello");
+        }
+    }
+}
